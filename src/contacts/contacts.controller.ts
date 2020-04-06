193d619028f1a3517e25767aa4d9da9eca4dde49
Vagus-art <agustin.favo@gmail.com>
2020-04-06 08:56:53 -0300

    added await

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -19,27 +19,27 @@ export class ContactsController {
 
   @Get('/id/:id')
   async getContactById(@Param('id') id: number): Promise<Contact> {
-    return this.CscService.getContactById(id);
+    return await this.CscService.getContactById(id);
   }
 
   @Get('/search/:search?')
   async getContactsBySearch(@Param('search') search?: string): Promise<Contact[]> {
-    return this.CscService.getContactsBySearch(search);
+    return await this.CscService.getContactsBySearch(search);
   }
 
   @Get('/:offset?')
   async getContactsDefault(@Param('offset') offset?: number): Promise<Contact[]> {
-    return this.CscService.getContactsDefault(offset);
+    return await this.CscService.getContactsDefault(offset);
   }
   
   @Post()
   async createContact(@Body() NewContact: CreateContactDto): Promise<ContactResponse> {
-    return { message: 'you have sent this', data: this.CscService.createContact(NewContact) };
+    return { message: 'you have sent this', data: await this.CscService.createContact(NewContact) };
   }
 
   @Put(':id')
   async updateContactById(@Body() UpdatedContact: CreateContactDto, @Param('id') id: number): Promise<ContactResponse> {
-    return { message: `you have updated the user : ${id}`, data: this.CscService.updateContactById(UpdatedContact,id) };
+    return { message: `you have updated the user : ${id}`, data: await this.CscService.updateContactById(UpdatedContact,id) };
   }
 
   @Delete(':id')
